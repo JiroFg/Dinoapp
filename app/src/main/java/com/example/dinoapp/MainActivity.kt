4873b2399c49c1e913791f07dcb9e55ed7ddfdeb
Christian <filetelopezchristian@gmail.com>
2023-03-17 14:47:08 -0600

    se realizó un cambio

--- app/src/main/java/com/example/dinoapp/MainActivity.kt
+++ app/src/main/java/com/example/dinoapp/MainActivity.kt
@@ -9,6 +9,6 @@ class MainActivity : AppCompatActivity() {
         installSplashScreen()
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        // aa
+        // aaaa
     }
 }
