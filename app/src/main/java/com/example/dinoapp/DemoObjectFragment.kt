08e8a79a93562a1e9092c7c362c9b082a4ffa073
Rousing <zs20018188@estudiantes.uv.mx>
2023-03-25 19:03:26 -0600

    Intento de alerta de internet :( mas otras cosas

--- app/src/main/java/com/example/dinoapp/DemoObjectFragment.kt
+++ app/src/main/java/com/example/dinoapp/DemoObjectFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import com.example.dinoapp.databinding.FragmentDemoObjectBinding
 
 
@@ -30,6 +31,20 @@ class DemoObjectFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
         arguments?.takeIf { it.containsKey(ARG_OBJECT) }?.apply {
             binding.textView.text = "Fragmento " + getInt(ARG_OBJECT).toString()
+
+            //LO QUE SUCEDE SI ES FRAGMENT 1 SEXO
+            if (getInt(ARG_OBJECT).toString() == "1"){
+                val toast = Toast.makeText(activity,"SEXO",Toast.LENGTH_SHORT).show()
+            }
+            //LO QUE SUCEDE SI ES FRAGMENT 2 NOMBRE
+            if (getInt(ARG_OBJECT).toString() == "2"){
+                val toast = Toast.makeText(activity,"NOMBRE",Toast.LENGTH_SHORT).show()
+            }
+
+            //LO QUE SUCEDE SI ES FRAGMENT 2 NOMBRE
+            if (getInt(ARG_OBJECT).toString() == "3"){
+                val toast = Toast.makeText(activity,"IMAGEN DE PERFIL",Toast.LENGTH_SHORT).show()
+            }
         }
     }
 }
