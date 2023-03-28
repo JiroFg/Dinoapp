636c176bf618b84f9a894a4218b6b9dc8884de5f
Rousing <zs20018188@estudiantes.uv.mx>
2023-03-27 18:20:54 -0600

    Metodo de internet con mensaje y boton

--- app/src/main/java/com/example/dinoapp/Home.kt
+++ app/src/main/java/com/example/dinoapp/Home.kt
@@ -4,55 +4,58 @@ import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.net.ConnectivityManager
+import android.net.NetworkCapabilities.NET_CAPABILITY_INTERNET
+import android.view.View.GONE
+import android.view.View.VISIBLE
+import android.widget.Button
+import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 
 
 class Home : AppCompatActivity() {
+
+    //    Conexion a internet
+    private lateinit var internetLayout: RelativeLayout
+    private lateinit var noInternetLayout: RelativeLayout
+    private lateinit var tryAgainButton: Button
+
     override fun onCreate(savedInstanceState: Bundle?) {
 
         //SPLASH SCREEN
         installSplashScreen()
 
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_home)
+        setContentView(R.layout.conexion)
 
-        // CONEXION A INTERNET
-        val textoConexion = findViewById<TextView>(R.id.mensaje)
+        supportActionBar?.hide()
+        internetLayout = findViewById(R.id.internetLayout)
+        noInternetLayout = findViewById(R.id.noInternetLayout)
+        tryAgainButton = findViewById(R.id.reintentar_boton)
 
-        Thread(Runnable {
-            while (true){
-                //AQUI SE DEBE PONER LO QUE DEBE ESTAR SI NO SE TIENE CONEXION A INTERNET
+        drawLayout()
 
-//                val dialog = error_conexion(
-//                    onSubmitClickListener = {
-//                        Toast.makeText(applicationContext,"Reintentando....", Toast.LENGTH_SHORT).show()
-//                    }
-//                )
+        tryAgainButton.setOnClickListener {
+            drawLayout()
+        }
+    }
 
+    private fun isNetworkAvailable(): Boolean {
+        val cm = getSystemService(CONNECTIVITY_SERVICE) as ConnectivityManager
+        val capabilities = cm.getNetworkCapabilities(cm.activeNetwork)
 
-                var textoInicial = "No tienes conexión"
-                //Toast.makeText(applicationContext,"Conectado :)", Toast.LENGTH_SHORT).show()
-                val conexion = getSystemService(Context.CONNECTIVITY_SERVICE) as ConnectivityManager
-                val leerInfo = conexion.allNetworkInfo
-                for(x in leerInfo){
-                    //NO IMPORTA SI ES CON DATOS O INTERNET, SOLO QUE ESTÉ CONECTADO
-                    //AQUI SE DEBE PONER LO QUE VA A SUCEDER SI SE TIENE CONEXION A INTERNET
-//                    if (x.typeName.equals("WIFI",ignoreCase = true))
-//                        if (x.isConnected) textoInicial = "Conectado con WIFI"
-//                    if (x.typeName.equals("MOBILE",ignoreCase = true))
-//                        if (x.isConnected) textoInicial = "Conectado con DATOS"
-                    if (x.typeName.equals("WIFI",ignoreCase = true) || (x.typeName.equals("MOBILE",ignoreCase = true)))
-                        if (x.isConnected){
-                            textoInicial = "Conectado con WIFI o con DATOS no importa"
-                            //Toast.makeText(applicationContext,"Conectado :)", Toast.LENGTH_SHORT).show()
-                        }
-                }
-                runOnUiThread{
-                    textoConexion.text = textoInicial
-                }
-            }
-        }).start()
+        return (capabilities != null && capabilities.hasCapability(NET_CAPABILITY_INTERNET))
+
+    }
+
+    private fun drawLayout() {
+        if (isNetworkAvailable()) {
+            internetLayout.visibility = VISIBLE
+            noInternetLayout.visibility = GONE
+        } else {
+            noInternetLayout.visibility = VISIBLE
+            internetLayout.visibility = GONE
+        }
     }
 }
