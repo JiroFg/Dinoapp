f6ad3b346c2c1dfb1092ab184dfa7b1d3b3b6b72
JiroFg <mechcromancer@gmail.com>
2023-03-29 20:16:47 -0600

    implementación de la pantalla sexo

--- app/src/main/java/com/example/dinoapp/ViewPagerAdapter.kt
+++ app/src/main/java/com/example/dinoapp/ViewPagerAdapter.kt
@@ -1,23 +1,21 @@
 package com.example.dinoapp
 
-import android.os.Bundle
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentActivity
 import androidx.viewpager2.adapter.FragmentStateAdapter
 
 class ViewPagerAdapter(fa: FragmentActivity) : FragmentStateAdapter(fa){
 
-    companion object {
-        private const val ARG_OBJECT = "object"
-    }
-
-    override fun getItemCount(): Int = 3
+    override fun getItemCount(): Int = 4
 
     override fun createFragment(position: Int): Fragment {
-        val fragment = DemoObjectFragment()
-        fragment.arguments = Bundle().apply {
-            putInt(ARG_OBJECT, position + 1)
+        var getFragment : Fragment? = null
+        when(position){
+            0->getFragment = BienvenidaFragment()
+            1->getFragment = SexoFragment()
+            2->getFragment = NombreFragment()
+            3->getFragment = ImagenFragment()
         }
-        return  fragment
+        return getFragment!!
     }
 }
