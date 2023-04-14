7cedafbd3336f35352ae933cbe791119206999cb
JiroFg <mechcromancer@gmail.com>
2023-04-13 21:53:01 -0600

    "Bloqueo de botones"

--- app/src/main/java/com/example/dinoapp/ImagenFragment.kt
+++ app/src/main/java/com/example/dinoapp/ImagenFragment.kt
@@ -5,55 +5,125 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
+import com.example.dinoapp.databinding.FragmentImagenBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ImagenFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ImagenFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding : FragmentImagenBinding? = null
+    private val binding get() = _binding!!
+    var isPressed:Boolean = false
+    var isPressed2:Boolean = false
+    var isPressed3:Boolean = false
+    var isPressed4:Boolean = false
+    var isPressed5:Boolean = false
+    var isPressed6:Boolean = false
+    var img:Int = 1
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_imagen, container, false)
+        val myInterface : InterfaceTransferencia = activity as InterfaceTransferencia
+        _binding = FragmentImagenBinding.inflate(inflater, container,false)
+        val view = binding.root
+        //Activar y desactivar image buttons
+        binding.imgBtn1.setOnClickListener {
+            if(isPressed){
+                binding.imgBtn1.setImageResource(R.drawable.jurassic_park)
+            }else{
+                binding.imgBtn1.setImageResource(R.drawable.ic_launcher_foreground)
+            }
+            //isPressed = !isPressed
+            exceptionButton(binding.imgBtn1)
+            img = 1
+        }
+        binding.imgBtn2.setOnClickListener {
+            if(isPressed2){
+                binding.imgBtn2.setImageResource(R.drawable.jurassic_park)
+            }else{
+                binding.imgBtn2.setImageResource(R.drawable.ic_launcher_foreground)
+            }
+            //isPressed2 = !isPressed2
+            exceptionButton(binding.imgBtn2)
+            img = 2
+        }
+        binding.imgBtn3.setOnClickListener {
+            if(isPressed3){
+                binding.imgBtn3.setImageResource(R.drawable.jurassic_park)
+            }else{
+                binding.imgBtn3.setImageResource(R.drawable.ic_launcher_foreground)
+            }
+            //isPressed3 = !isPressed3
+            exceptionButton(binding.imgBtn3)
+            img = 3
+        }
+        binding.imgBtn4.setOnClickListener {
+            if(isPressed4){
+                binding.imgBtn4.setImageResource(R.drawable.jurassic_park)
+            }else{
+                binding.imgBtn4.setImageResource(R.drawable.ic_launcher_foreground)
+            }
+            //isPressed4 = !isPressed4
+            exceptionButton(binding.imgBtn4)
+            img = 4
+        }
+        binding.imgBtn5.setOnClickListener {
+            if(isPressed5){
+                binding.imgBtn5.setImageResource(R.drawable.jurassic_park)
+            }else{
+                binding.imgBtn5.setImageResource(R.drawable.ic_launcher_foreground)
+            }
+            //isPressed5 = !isPressed5
+            exceptionButton(binding.imgBtn5)
+            img = 5
+        }
+        binding.imgBtn6.setOnClickListener {
+            if(isPressed6){
+                binding.imgBtn6.setImageResource(R.drawable.jurassic_park)
+            }else{
+                binding.imgBtn6.setImageResource(R.drawable.ic_launcher_foreground)
+            }
+            //isPressed6 = !isPressed6
+            exceptionButton(binding.imgBtn6)
+            img = 6
+        }
+
+        binding.btn.setOnClickListener {
+            myInterface.transferirImg(img)
+        }
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ImagenFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ImagenFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroy() {
+        super.onDestroy()
+        _binding = null
+    }
+
+    fun exceptionButton(boton:ImageButton){
+        if(boton != binding.imgBtn1){
+            binding.imgBtn1.setImageResource(R.drawable.jurassic_park)
+            isPressed = false
+        }
+        if(boton != binding.imgBtn2){
+            binding.imgBtn2.setImageResource(R.drawable.jurassic_park)
+            isPressed2 = false
+        }
+        if(boton != binding.imgBtn3){
+            binding.imgBtn3.setImageResource(R.drawable.jurassic_park)
+            isPressed3 = false
+        }
+        if(boton != binding.imgBtn4){
+            binding.imgBtn4.setImageResource(R.drawable.jurassic_park)
+            isPressed4 = false
+        }
+        if(boton != binding.imgBtn5){
+            binding.imgBtn5.setImageResource(R.drawable.jurassic_park)
+            isPressed5 = false
+        }
+        if(boton != binding.imgBtn6){
+            binding.imgBtn6.setImageResource(R.drawable.jurassic_park)
+            isPressed6 = false
+        }
     }
 }
--- app/src/main/java/com/example/dinoapp/MainActivity.kt
+++ app/src/main/java/com/example/dinoapp/MainActivity.kt
@@ -5,13 +5,14 @@ import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
-import android.widget.Toast
-import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.example.dinoapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity(), InterfaceTransferencia {
 
     private lateinit var binding: ActivityMainBinding
+    private var sexo: Boolean = false
+    private var nombre: String = ""
+    private var img: Int = 1
 
     lateinit var preference : SharedPreferences
     val prefShowIntro = "Intro"
@@ -19,44 +20,43 @@ class MainActivity : AppCompatActivity(), InterfaceTransferencia {
     private val adapter by lazy { ViewPagerAdapter(this) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
-
-        //SPLASH SCREEN
-        installSplashScreen()
-
         super.onCreate(savedInstanceState)
-
-        //VIEW DE BIENVENIDA
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
+        //VIEW DE BIENVENIDA
         preference = getSharedPreferences("MisPreferencias", MODE_PRIVATE)
-
+        //si las preferencias estan por defecto "true" redirige al adapter
         if(preference.getBoolean(prefShowIntro, true)) {
-
-            val editor = preference.edit()
-            editor.putBoolean(prefShowIntro, false)
-            editor.commit()
             binding.pager.adapter = adapter
-
-        }else {
+        }else{
+            //caso contrario nos redirige a la pantalla home
             val intent = Intent(this, Home::class.java)
             startActivity(intent)
             finish()
         }
-
     }
 
-    override fun transferirSexo(msg: Boolean) {
-        Toast.makeText(this,msg.toString(), Toast.LENGTH_SHORT).show()
-        Log.d("valorSexo: ",msg.toString())
+    override fun transferirSexo(sexo: Boolean) {
+        Log.d("valorSexo: ",sexo.toString())
+        this.sexo = sexo
     }
 
     override fun transferirNombre(nombre: String) {
-        Toast.makeText(this,nombre, Toast.LENGTH_SHORT).show()
         Log.d("valorNombre: ",nombre)
+        this.nombre = nombre
     }
 
-    override fun transferirImg(img: String) {
-
+    override fun transferirImg(img: Int) {
+        //Log.d("valorImg: ",img.toString())
+        this.img = img
+        //modificación de preferencias al finalizar la configuración del usuario
+        val editor = preference.edit()
+        //cambia a false para que no se vuelva a mostrar la configuración inicial
+        editor.putBoolean(prefShowIntro, false)
+        editor.apply()
+        //coloca los datos en el intent y redirige a la pantalla home
+        val intent = Intent(this, Home::class.java)
+        startActivity(intent)
+        finish()
     }
 }
