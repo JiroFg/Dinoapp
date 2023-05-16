2d0774d6d8ed9981f7e10a88e45710772eed7f9e
Rousing <zs20018188@estudiantes.uv.mx>
2023-05-15 23:58:34 -0600

    Ruleta

--- app/src/main/java/com/example/dinoapp/fragment/FShop.kt
+++ app/src/main/java/com/example/dinoapp/fragment/FShop.kt
@@ -5,56 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.dinoapp.R
-
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [FShop.newInstance] factory method to
- * create an instance of this fragment.
- */
+import android.widget.Toast
+import com.example.dinoapp.databinding.FragmentFShopBinding
 class FShop : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private var _binding: FragmentFShopBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
     }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_f_shop, container, false)
-    }
+        _binding = FragmentFShopBinding.inflate(inflater,container,false)
+        binding.btnRuleta.setOnClickListener {
+            val randomNumber = (1..1000).random()
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment FShop.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            FShop().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
+            when(randomNumber) {
+                in 1..600 -> Toast.makeText(context, "Comun", Toast.LENGTH_SHORT).show()
+                in 601..800 -> Toast.makeText(context, "No común", Toast.LENGTH_SHORT).show()
+                in 801..949 -> Toast.makeText(context, "Raro", Toast.LENGTH_SHORT).show()
+                in 950..1000 -> Toast.makeText(context, "Legendario", Toast.LENGTH_SHORT).show()
             }
+        }
+
+        return binding.root
     }
 }
