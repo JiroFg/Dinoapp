bd6360faa75448a54d3c45d32486df045f839518
Rousing <zs20018188@estudiantes.uv.mx>
2023-05-23 00:40:43 -0600

    Ruleta animacion y monedas

--- app/src/main/java/com/example/dinoapp/fragment/FShop.kt
+++ app/src/main/java/com/example/dinoapp/fragment/FShop.kt
@@ -11,6 +11,8 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.view.Window
+import android.view.animation.Animation
+import android.view.animation.RotateAnimation
 import android.widget.Button
 import android.widget.ImageView
 import android.widget.TextView
@@ -27,6 +29,7 @@ import com.example.dinoapp.Prefs.Prefs
 import com.example.dinoapp.R
 import com.example.dinoapp.ShopRecycler.ShopAdapter
 import com.example.dinoapp.databinding.FragmentFShopBinding
+import java.util.Random
 
 class FShop : Fragment() {
 
@@ -42,35 +45,16 @@ class FShop : Fragment() {
         _binding = FragmentFShopBinding.inflate(inflater, container, false)
         prefs = Prefs(binding.btnRuleta.context)
         //metodo para colocar la información del usuario en la TopBar
-        addInformationUser()
         //Listener cuando presionas el botón para girar la ruleta
         binding.btnRuleta.setOnClickListener {
-            //Genera un valor random entre 1 y 1000
-            val randomNumber = (1..1000).random()
-            //En un When dependiendo el valor obtenido da un premio u otro
-            when (randomNumber) {
-                in 1..600 -> {
-                    Toast.makeText(context, "Comun", Toast.LENGTH_SHORT).show()
-                    showDialogPremio1()
-                }
-
-                in 601..800 -> {
-                    Toast.makeText(context, "No común", Toast.LENGTH_SHORT).show()
-                    showDialogPremio2()
-                }
-
-                in 801..949 -> {
-                    Toast.makeText(context, "Raro", Toast.LENGTH_SHORT).show()
-                    showDialogPremio3()
-                }
-
-                in 950..1000 -> {
-                    Toast.makeText(context, "Legendario", Toast.LENGTH_SHORT).show()
-                    showDialogPremio4()
-                }
-            }
+            girarRuleta()
+        }
+        // boton que muestra los premios de la ruleta
+        binding.Premios.setOnClickListener {
+            showDialogPremios()
         }
         initRecyclerView()
+        addInformationUser()
         return binding.root
     }
     //metodo para inicializar el RecyclerView que dibuja los dinosaurios disponibles en la tienda
@@ -168,6 +152,8 @@ class FShop : Fragment() {
 //        dialog.setCancelable( true )
         dialog.setContentView(R.layout.dialog_premio2)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+        prefs.editCoins(prefs.getCoins()+20)
+        Toast.makeText(context, prefs.getCoins().toString(), Toast.LENGTH_SHORT).show()
 
         val dialogButton: Button = dialog.findViewById(R.id.dialog_aceptar)
         dialogButton.setOnClickListener {
@@ -182,6 +168,8 @@ class FShop : Fragment() {
 //        dialog.setCancelable( true )
         dialog.setContentView(R.layout.dialog_premio3)
         dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        prefs.editCoins(prefs.getCoins()+30)
 
         val dialogButton: Button = dialog.findViewById(R.id.dialog_aceptar)
         dialogButton.setOnClickListener {
@@ -202,6 +190,79 @@ class FShop : Fragment() {
             dialog.dismiss()
         }
         dialog.show()
+    }
+
+    private fun showDialogPremios(){
+        val dialog = Dialog(requireContext())
+        dialog.requestWindowFeature(Window.FEATURE_NO_TITLE)
+//        dialog.setCancelable( true )
+        dialog.setContentView(R.layout.dialog_premios)
+        dialog.window?.setBackgroundDrawable(ColorDrawable(Color.TRANSPARENT))
+
+        val dialogButton: Button = dialog.findViewById(R.id.dialog_aceptar)
+        dialogButton.setOnClickListener {
+            dialog.dismiss()
+        }
+        dialog.show()
+    }
+    private fun girarRuleta(){
+        //Genera un valor random entre 1 y 1000
+        val randomNumber = (1..1000).random()
+
+        val rouletteImageView = binding.ruletacentro
+        val rouletteSections = listOf(randomNumber)
+        val random = Random()
+        val degrees = random.nextInt(360) + 720 // Gira al menos 2 vueltas completas (720 grados)
+
+        val rotateAnimation = RotateAnimation(0f, degrees.toFloat(), Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f)
+        rotateAnimation.duration = 3000 // Duración de la animación (en milisegundos)
+        rotateAnimation.fillAfter = true
+
+        rouletteImageView.startAnimation(rotateAnimation)
+
+        rotateAnimation.setAnimationListener(object : Animation.AnimationListener {
+            override fun onAnimationStart(animation: Animation) {
+                binding.btnRuleta.isClickable = false
+                binding.btnRuleta.isEnabled = false
+                binding.btnRuleta.setBackgroundResource(R.drawable.bg_button_racha)
+            }
+
+            override fun onAnimationEnd(animation: Animation) {
+                // Lógica para mostrar el resultado
+                val resultIndex = random.nextInt(rouletteSections.size)
+                val result = rouletteSections[resultIndex]
+//                    Toast.makeText(context, "¡Resultado: $result!", Toast.LENGTH_SHORT).show()
+                //En un When dependiendo el valor obtenido da un premio u otro
+                when (result) {
+                    in 1..600 -> {
+                        Toast.makeText(context, "Comun", Toast.LENGTH_SHORT).show()
+                        showDialogPremio1()
+                    }
+
+                    in 601..800 -> {
+                        Toast.makeText(context, "No común", Toast.LENGTH_SHORT).show()
+                        showDialogPremio2()
+                    }
+
+                    in 801..949 -> {
+                        Toast.makeText(context, "Raro", Toast.LENGTH_SHORT).show()
+                        showDialogPremio3()
+                    }
+
+                    in 950..1000 -> {
+                        Toast.makeText(context, "Legendario", Toast.LENGTH_SHORT).show()
+                        showDialogPremio4()
+                    }
+                }
+
+                binding.btnRuleta.isClickable = true
+                binding.btnRuleta.isEnabled = true
+                binding.btnRuleta.setBackgroundResource(R.drawable.btn)
+            }
+
+            override fun onAnimationRepeat(animation: Animation) {
+            }
+        })
     }
 
     fun addInformationUser() {
