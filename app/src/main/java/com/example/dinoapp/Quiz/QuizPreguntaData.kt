24882aa4ba64774ce0d9efd06bd18acf30938ae8
JiroFg <mechcromancer@gmail.com>
2023-05-24 16:08:33 -0600

    "Consumo del quiz"

--- app/src/main/java/com/example/dinoapp/Quiz/QuizPreguntaData.kt
+++ app/src/main/java/com/example/dinoapp/Quiz/QuizPreguntaData.kt
@@ -3,9 +3,9 @@ package com.example.dinoapp.Quiz
 data class QuizPreguntaData (
     var id:Int,
     var pregunta:String,
-    var opcion_uno:String,
-    var opcion_dos:String,
-    var opcion_tres:String,
-    var opcion_cuatro:String,
+    var resUno:String,
+    var resDos:String,
+    var resTres:String,
+    var resCuatro:String,
     var correcta:Int
         )
--- app/src/main/java/com/example/dinoapp/Quiz/QuizProvider.kt
+++ app/src/main/java/com/example/dinoapp/Quiz/QuizProvider.kt
@@ -2,7 +2,6 @@ package com.example.dinoapp.Quiz
 
 import android.util.Log
 import com.example.dinoapp.APIService
-import com.example.dinoapp.HomeActivity
 import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
@@ -12,24 +11,26 @@ import retrofit2.converter.gson.GsonConverterFactory
 class QuizProvider {
 
     companion object {
-        fun cargarQuices() {
+        fun cargarQuiz(id: Int): MutableList<QuizPreguntaData>{
+            val quizData = mutableListOf<QuizPreguntaData>()
             val retrofit: Retrofit = Retrofit.Builder()
                 .baseUrl("https://dinoapi-bior.onrender.com/")
                 .addConverterFactory(GsonConverterFactory.create())
                 .build()
             val service = retrofit.create<APIService>(APIService::class.java)
-            service.getQuices().enqueue(object : Callback<List<QuizPreguntaData>> {
+            service.getQuiz(id).enqueue(object : Callback<List<QuizPreguntaData>> {
                 override fun onResponse(call: Call<List<QuizPreguntaData>>, response: Response<List<QuizPreguntaData>>) {
                     val lessons = response.body()
                     lessons?.forEach {
                         Log.d("QUICES", it.toString())
-                        HomeActivity.quizData.add(it)
+                        quizData.add(it)
                     }
                 }
                 override fun onFailure(call: Call<List<QuizPreguntaData>>, t: Throwable) {
                     t?.printStackTrace()
                 }
             })
+            return quizData
         }
     }
 
